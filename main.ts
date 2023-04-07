e29c1c8d012caff9ebdac8f679fab168aeca06f7
lacz83 <lacz83@hotmail.com>
2023-04-07 01:22:41 +0000

    Commit Tarea 6

--- main.ts
+++ main.ts
@@ -11,9 +11,11 @@ console.log(ap.cursos);
 
 let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
 let estadisticasTable: HTMLElement = document.getElementById("estadisticas")!;
+let cursosTable: HTMLElement = document.getElementById("cursos")!;
 
 mostrarDatosAprendiz(ap);
 mostrarEstadisticas(ap);
+mostrarCursosAprendiz(ap);
 
 function mostrarDatosAprendiz(aprendiz: Aprendiz): void {
     let tbodyAprendiz = document.createElement("tbody");
@@ -31,3 +33,18 @@ function mostrarEstadisticas(aprendiz:Aprendiz):void {
     trElement.innerHTML = `<td><b>Cursos Certificados</b></td><td>${numeroCertificados}</td>`;
     estadisticasTable.appendChild(trElement);
 } 
+
+function mostrarCursosAprendiz(aprendiz: Aprendiz): void {
+    let cursosTbody: HTMLElement = document.createElement("tbody");
+    for(let curso of aprendiz.cursos)
+    {
+        let trElement: HTMLElement = document.createElement("tr");
+        trElement.innerHTML=`<td>${curso.nombre}</td>
+        <td>${curso.horas}</td>
+        <td>${curso.calificacion}</td>
+        <td>${curso.certificado}</td>
+        <td>${curso.anio}</td>`;
+        cursosTbody.appendChild(trElement);
+    }
+    cursosTable.appendChild(cursosTbody);
+}
